5ce533345c296a3517cd9a118237367bc50e26e6
Sua Kang <rkdtndk09@gmail.com>
2023-03-25 16:10:00 +0900

    hotfix: insert시 table에 null값 저장 수정

--- temp/src/main/java/com/example/springclassdemo/product/domain/ProductEntity.java
+++ temp/src/main/java/com/example/springclassdemo/product/domain/ProductEntity.java
@@ -1,2 +1,25 @@
-package com.example.springclassdemo.product.domain;public class ProductEntity {
+package com.example.springclassdemo.product.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Table
+@Entity
+@Getter
+@NoArgsConstructor
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private Long price;
+
+    public ProductEntity(String name, Long price) {
+        this.name = name;
+        this.price = price;
+    }
 }
